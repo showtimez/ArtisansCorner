2fd982851890fbed5473a8b252ba312b83e80aac
Sara Pileio <sarapileio@gmail.com>
2023-05-22 15:25:24 +0200

    creato db, migrazione standard di laravel, inizializzazione, creato publicController, bundeling degli assets, creata cartella components con layout,navbar,header,footer, creata welcome e inserito direttiva vite

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title> Artisan Corner </title>
+  </head>
+  <body>
+    <x-navbar />
+    <x-header header="{{$header}}"/>
+
+    {{$slot}}
+
+
+    <x-footer />
+
+
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<x-layout header="HomePage">
+
+        <h1>  Olaa </h1>
+
+</x-layout>
