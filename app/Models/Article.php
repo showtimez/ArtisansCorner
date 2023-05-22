58eea37f30beb49b98e2ec71ea53590625e4004b
PaleseGiulio <128043856+PaleseGiulio@users.noreply.github.com>
2023-05-22 17:41:49 +0200

    implementato modello e migrazione article

---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'category',
+        'title',
+        'price',
+        'description',
+        'image',
+        'state',
+    ];
+}
