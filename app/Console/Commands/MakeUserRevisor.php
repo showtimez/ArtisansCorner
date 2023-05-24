7ccdd472ffa1b0481b798af25b82e2f6390b2851
Lucia Pacciolla <pacciolla.lucia@libero.it>
2023-05-24 17:09:10 +0200

    creata colonna e comando revisore

---
+++ app/Console/Commands/MakeUserRevisor.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserRevisor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'corner:makeUserRevisor {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user=User::where('email', $this->argument('email'))->first();
+        if(!$user){
+            $this->error('Utente non trovato');
+            return;
+        }
+
+        $user->is_revisor=true;
+        $user->save();
+        $this->info("L'utente {$user->name} è ora un revisore");
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,15 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MakeUserRevisor;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        MakeUserRevisor::class,
+    ];
     /**
      * Define the application's command schedule.
      */
