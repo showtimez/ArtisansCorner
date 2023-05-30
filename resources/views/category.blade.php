e0239534b09e17551106aa73ab98cdde44fb60f3
Sara Pileio <sarapileio@gmail.com>
2023-05-30 17:31:03 +0200

    inserita flip card per singole categorie

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -7,69 +7,43 @@
         </div>
             {{-- <span id="testo"></span> --}}
 
-        <script type="text/javascript">
-            // testo da mostrare
-            var testo = "Ecco tutti gli annunci di :";
-            // output
-            var output = "";
-            // incrementatore
-            var i = 0;
-            // velocità di scrittura
-            var speed = 120;
-            // dichiaro la funzione
-            function scrivi() {
-                // creo l'output
-                output += testo.charAt(i);
-                // incremento
-                i++;
-                // scrittura
-                document.getElementById("testo").innerHTML = output;
-                // se è finito il testo
-                if(i >= testo.length) {
-                    // fine
-                    clearInterval(s);
-                }
-            }
-            // richiamo la funzione a intervalli
-            s = setInterval("scrivi()",speed);
-        </script>
-
+        
 
 
     </x-header>
-    <div class="container-fluid vh-100">
-        <div class="row justify-content-center py-5 my-5">
-            <div class="col-12 col-md-4">
+    <div class="container-fluid ">
+        <div class="row ">
+            <div class="col-12 col-md-4 d-flex align-items-center colC">
 
                 @forelse ($articles as $article)
-                    <div class="card my-3">
-
-                        {{-- <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="foto di {{$article->title}}"> --}}
-                        <div class="card-body ">
-                            <h5 class="card-title text-center">{{ $article->title }}</h5>
-                            <p class="card-text text-center">{{ $article->description }}</p>
+                    
+                    <div class="flip-card  m-5 ">    
+                        <div class="flip-card-inner ">
+                          <div class="flip-card-front d-flex justify-content-around rounded">
+                            <img class="imgC rounded"src="{{!$article->images()->get()->isEmpty() ? $article->images()->first()->getUrl(400,300) : url('./media/noImg.png')}}" alt="no img">
+                          </div>
+                          <div class="flip-card-back rounded">
+                            <h5 class="mt-5">{{ $article->title }}</h5>
+                            <p>{{ $article->description }}</p>
                             <p>Stato: {{ $article->state }}</p>
 
-                            <div class=" container-fluid d-flex gap-5">
-                                <h4>Prezzo: {{ $article->price }}</h4>
-                                <p class="mx-0">Categoria {{ $article->category->name }}</p>
-                            </div>
-                            {{-- <a href="{{route('article.show')}}" class="btn btn-primary">Dettagli</a> --}}
+                            <h4>Prezzo: {{ $article->price }}</h4>
+                            <p class="mx-0">Categoria {{ $article->category->name }}</p>
+                            <a href="{{route('article.show', compact('article'))}}" class="btn linkcustomAccent  px-5 shadow">{{ __('ui.dettagli') }}</a>
+                          </div>
                         </div>
                     </div>
-                @empty
-                    <div class="d-flex justify-content-center ">
-                        <h3>Non sono ancora stati inseriti annunci.</h3>
-                    </div>
-                @endforelse
+
+                      @empty
+                      <div class="d-flex justify-content-center ">
+                          <h3>Non sono ancora stati inseriti annunci.</h3>
+                      </div>
+                      @endforelse
+  
 
             </div>
+          
         </div>
     </div>
-
-
-
-
-
 
 </x-layout>
