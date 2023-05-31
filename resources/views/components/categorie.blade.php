0f727d48279cd129d775d40e8cb250693cc24278
Sara Pileio <sarapileio@gmail.com>
2023-05-31 12:29:19 +0200

    migliorato carosello welcome

--- resources/views/components/categorie.blade.php
+++ resources/views/components/categorie.blade.php
@@ -13,14 +13,14 @@
             <span class="icon"><i class="fa-solid fa-palette"></i></span>
             </a>
             <p class="text-center  fs-5 fw-bold">{{ __('ui.categorieIcone2') }}</p>
-            <p class="text-center textDark px-5  fs-7">La musica accompagna l’uomo dall’alba dei tempi.</p>
+            <p class="text-center textDark px-5  fs-7">Prima sogno i miei dipinti, poi dipingo i miei sogni.</p>
         </div>
         <div class="col-12 col-md-3 d-flex flex-column align-items-center py-5">
             <a href="">
             <span class="icon"><i class="fa-solid fa-camera"></i></span>
             </a>
             <p class="text-center  fs-5 fw-bold">{{ __('ui.categorieIcone3') }}</p>
-            <p class="text-center textDark px-5 fs-7">La musica accompagna l’uomo dall’alba dei tempi.</p>
+            <p class="text-center textDark px-5 fs-7">La fotografia è un arte; anzi è più che un’arte.</p>
         </div>
         <div class="col-12 col-md-3 d-flex flex-column align-items-center py-5">
             <a href="{{route('article.index')}}">
@@ -29,7 +29,7 @@
             </a>
             <p class="text-center fs-5 fw-bold">{{ __('ui.categorieIcone4') }}</p>
 
-            <p class="text-center textDark px-5 fs-7">La musica accompagna l’uomo dall’alba dei tempi.</p>
+            <p class="text-center textDark px-5 fs-7">Scopri di più su tutte le categorie disponibili nel sito.</p>
         </div>
         <hr>
     </div>
